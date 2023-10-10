3481afce0250cc5a2289079310bab9b960d12447
ArturoOS <Arturo_Osorio1@epam.com>
2023-10-10 09:57:23 -0600

    Module1

---
+++ HelloCore/HelloCore/Hello.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloCore
+{
+	class Hello
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("MODULE 1: .NET FUNDAMENTALS");
+			Console.Write("\nINPUT, USERNAME:");
+			string username = Console.ReadLine();
+			string output = HelloLibrary.HelloLibrary.HelloConcat(username);
+			Console.WriteLine(output);
+			Console.ReadLine();
+		}
+	}
+}
---
+++ HelloLibrary/HelloLibrary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloLibrary
+{
+	public static class HelloLibrary
+	{
+		public static string HelloConcat(string username) 
+		{
+			if (string.IsNullOrEmpty(username))
+				return "Invalid username.";
+			else 
+			{
+				string currentTime = DateTime.Now.ToString("g");
+				return $"{currentTime} Hello, {username}.";
+			}
+		}
+	}
+}
